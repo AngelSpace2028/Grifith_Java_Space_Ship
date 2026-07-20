ee09d40176d70f614e2ea9c945001946ad572d75
AngelSpace2028 <pacalovasjurijus517@gmail.com>
2026-07-20 12:13:08 +0100

    Change package name from griffin to ie.Griffith

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
-package griffin;
+package ie.Griffith;
 
 import processing.core.PApplet;
 
 public class Main {
     public static void main(String[] args) {
-        PApplet.main("griffin.SpaceshipGame");
+        PApplet.main("ie.Griffith.SpaceshipGame");
     }
 }
